1817dd10fb9b61b3528356447ffafb9263f00d4d
Cesar Aldair Garcia Velez <aldairgarciavelez1@gmail.com>
2023-03-19 23:49:24 -0600

    Eliminadas cosas innecesarias

--- app/src/main/java/com/example/mimedicokotlinformedics/ui/videochat/JavascriptInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mimedicokotlinformedics.ui.videochat
-
-
-class JavascriptInterface(private val videochatFragment: VideochatFragment) {
-    @android.webkit.JavascriptInterface
-    fun onPeerConnected(){
-        videochatFragment.onPeerConnected()
-    }
-}
--- app/src/main/java/com/example/mimedicokotlinformedics/ui/videochat/VideochatData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mimedicokotlinformedics.ui.videochat
-
-data class VideochatData (
-    val uniqueId: String?,
-    val timestamp: Long
-)
